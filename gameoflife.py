54494af3a42c3183a48f6b9c7a725ab18d28a59e
Blake <itsmazzyy@gmail.com>
2024-03-31 00:10:17 +0000

    added matrix & temp camera movement

--- gameoflife.py
+++ gameoflife.py
@@ -1,5 +1,8 @@
 import pygame
+import numpy as np
 
+GAME_WIDTH = 1920
+GAME_HEIGHT = 1080
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 # colour scheme credit to catppuccin @ catppuccin.com
@@ -7,23 +10,49 @@ BACKGROUND = (36, 39, 58)
 GRID_COLOR = (73, 77, 100)
 
 def main():
-    global SCREEN, CLOCK, RUNNING
+    global SCREEN, CLOCK, RUNNING, GAME_SURF, camera_x, camera_y
     pygame.init()
-    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE, vsync=1)
+    GAME_SURF = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
     CLOCK = pygame.time.Clock()
     RUNNING = True
     CELL_SIZE = 20
-
-    SCREEN.fill(BACKGROUND)
-    createGrid(CELL_SIZE)
+    camera_x = 0
+    camera_y = 0
+    
+    grid = createGrid(CELL_SIZE)
 
     while RUNNING:
-        for cell in grid:
-            cell.draw()
+        SCREEN.fill(BACKGROUND)
+        GAME_SURF.fill(BACKGROUND)
+        screen_rect = SCREEN.get_rect()
+        surf_rect = GAME_SURF.get_rect() 
+        for row in grid:
+            for cell in row:
+                cell.draw()
+        pygame.draw.rect(GAME_SURF, GRID_COLOR, (0, 0, GAME_WIDTH, GAME_HEIGHT), 2) # draw a border around the entire game grid
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            camera_x += 5
+        elif keys[pygame.K_RIGHT]:
+            camera_x -= 5
+        elif keys[pygame.K_DOWN]:
+            camera_y -= 5
+        elif keys[pygame.K_UP]:
+            camera_y += 5
+        
+        surf_rect.center = screen_rect.center # set the area of the game surface to the centre of the main screen surface
+        surf_rect.x += camera_x
+        surf_rect.y += camera_y
+        SCREEN.blit(GAME_SURF, surf_rect)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 RUNNING = False
-
+            elif event.type == pygame.VIDEORESIZE:
+                SCREEN = pygame.display.set_mode(event.size, pygame.RESIZABLE, vsync=1)
+                pygame.transform.scale(GAME_SURF, event.size)
+                
         pygame.display.flip()
         CLOCK.tick(60)
 
@@ -36,13 +65,17 @@ class Cell:
         self.live = False
         self.coords = (x // size, y // size) # dividing a cell's screen position by its size results in its actual grid position
     def draw(self):
-        pygame.draw.rect(SCREEN, self.colour, (self.x, self.y, self.size, self.size), 1)
+        pygame.draw.rect(GAME_SURF, self.colour, (self.x, self.y, self.size, self.size), 1)
 
-grid = []
 def createGrid(size):
-    for x in range(0, WINDOW_WIDTH, size):
-        for y in range(0, WINDOW_HEIGHT, size):
-            grid.append(Cell(x, y, size))
+    grid_width = GAME_WIDTH // size
+    grid_height = GAME_HEIGHT // size
+    grid = np.empty(shape=(grid_width, grid_height), dtype=object)
+    for x in range(grid_width):
+        for y in range(grid_height):
+            grid[x][y] = Cell((x * size), (y * size), size)
+    return grid
+            
 
 main()
 pygame.quit()
